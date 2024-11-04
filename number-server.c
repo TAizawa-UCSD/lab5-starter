b74558aae16bb2a3ca301d4b2291eeb6d638a976
TAizawa-UCSD <taaizawa@ucsd.edu>
2024-11-04 18:50:57 +0000

    Week6_110424

--- number-server.c
+++ number-server.c
@@ -4,6 +4,7 @@
 int num = 0;
 
 char const HTTP_404_NOT_FOUND[] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
+char const HTTP_200_OK[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
 
 void handle_404(int client_sock, char *path)  {
     printf("SERVER LOG: Got request for unrecognized path \"%s\"\n", path);
@@ -13,6 +14,23 @@ void handle_404(int client_sock, char *path)  {
     // snprintf includes a null-terminator
 
     // TODO: send response back to client?
+    write(client_sock, HTTP_404_NOT_FOUND, strlen(HTTP_404_NOT_FOUND)); //added
+    write(client_sock, response_buff, strlen(response_buff));
+}
+
+void handle_root(int client_sock){
+    char message[BUFFER_SIZE];
+    snprintf(message, BUFFER_SIZE, "Surrent number: %d\n", num);
+    write(client_sock, HTTP_200_OK, strlen(HTTP_200_OK));
+    write(client_sock, message, strlen(message));
+}
+
+void handle_increment(int client_sock){
+    char message[BUFFER_SIZE];
+    num +=1;
+    snprintf(message, BUFFER_SIZE, "Incremented to: %d\n", num);
+    write(client_sock, HTTP_200_OK, strlen(HTTP_200_OK));
+    write(client_sock, message, strlen(message));
 }
 
 
@@ -27,7 +45,21 @@ void handle_response(char *request, int client_sock) {
         return;
     }
 
-    handle_404(client_sock, path);
+
+    /*
+        "/" shows "Current number: ____"
+        "/increment" - adds 1 to number and shows "Incremented to: ____"
+    */
+
+    if(strcmp(path,"/") == 0){
+        printf("Here we will handle the root path / \n");
+        handle_root(client_sock);
+        return;
+    } else if(strcmp(path, "/increment") == 0){
+        handle_increment(client_sock);
+    } else {
+        handle_404(client_sock, path);
+    }
 }
 
 int main(int argc, char *argv[]) {
